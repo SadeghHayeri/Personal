f36e6502e3187991e4966ed704e7defe32f95103
SadeghHayeri <hayerisadegh@gmail.com>
2017-03-02 01:33:56 +0330

    counter added + neuronCtrl added

--- university/Computer Architecture/CA1/src/Counter.v
+++ university/Computer Architecture/CA1/src/Counter.v
@@ -1,13 +1,44 @@
-module Counter(clk, en, reset, out);
-  parameter width = 4;
+module Counter(clk, en, rst, out);
+
+  parameter [2:0] start = 0, init = 1, getData = 2, changeWeight = 3, doneCheck = 4, reset = 5;
+  reg [2:0] ns = 0, ps = 0;
+
+  always @ ( posedge clk, posedge rst ) begin
+    if( rst == 1'b1 )
+        ps <= start;
+    else
+        ps <= ns;
+  end
+
+  always @ ( ps ) begin
+
+    done = 1'b1;
+    reset = 1'b0;
+    
+      Rshift = 1'b0;
+      Rload = 1'b0;
+      Rreset = 1'b0;
+      Qshift = 1'b0;
+      Qload = 1'b0;
+      Mload = 1'b0;
+      Cup = 1'b0;
+      Creset = 1'b0;
+      done = 1'b0;
+
+      case (ps)
+          waitOnStart1: done = 1'b1;
+          waitOnStart0:;
+          getInput: begin Rreset = 1'b1; Qload = 1'b1; Mload = 1'b1; Creset = 1'b1; end
+          shiftR: Rshift = 1'b1;
+          shiftQ: begin Qshift = 1'b1; Rload = 1'b1; end
+          countUp: Cup = 1'b1;
+      endcase
+  end
+
+
+
 
   input clk, en, reset;
-  output reg [width-1:0] out;
-
-  always @ (posedge clk)
-    if(reset)
-      out <= 0;
-    else
-      out <= out + 1;
+  output reg [3:0] out;
 
 endmodule // end counter
--- university/Computer Architecture/CA1/src/NeuronCtrl.v
+++ university/Computer Architecture/CA1/src/NeuronCtrl.v
@@ -1,1 +1,48 @@
 
+module Controller( input clk, rst, start, counterCarryOut, output reg Rshift, Rload, Rreset, Qshift, Qload, Mload, Cup, Creset, done );
+
+	parameter [2:0] start = 0, init = 1, getData = 2, changeWeight = 3, doneCheck = 4, reset = 5;
+  reg [2:0] ns = 0, ps = 0;
+
+  always @ ( posedge clk, posedge rst ) begin
+    if( rst == 1'b1 )
+      ps <= waitOnStart1;
+    else
+      ps <= ns;
+  end
+
+  always @ ( ps ) begin
+
+		done = 1'b1;
+		reset = 1'b0;
+		ldRegx1 = 1'b0;
+		ldRegx2 = 1'b0;
+		ldRegT = 1'b0;
+		ldRegW1 = 1'b0;
+		ldRegW2 = 1'b0;
+		ldRegFlag = 1'b0;
+
+    case (ps)
+      start:;
+      init:;
+      getData:;
+      changeWeight:;
+      doneCheck:;
+      reset:;
+    endcase
+
+  end
+
+  always @ ( ps, start, counterCarryOut ) begin
+    ns = start;
+    case (ps)
+      start: ns = (start == 1'b1)? waitOnStart0 : waitOnStart1;
+      init: ns = ;
+      getData: ns = ;
+      changeWeight: ns = ;
+      doneCheck: ns = ;
+      reset: ns = ;
+    endcase
+  end
+
+endmodule
