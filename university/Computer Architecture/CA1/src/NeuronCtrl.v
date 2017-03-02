49d739a66c00c85c70e3a8fe229c0c8f687670c3
Rouzbeh <Rouzbeh@Aqa-Rouzbeh.local>
2017-03-02 17:38:18 +0330

    Cu+HandShaking

--- university/Computer Architecture/CA1/src/NeuronCtrl.v
+++ university/Computer Architecture/CA1/src/NeuronCtrl.v
@@ -1,7 +1,10 @@
 
-module Controller(clk, rst, start, endFlag, yEqualt, flagEOF, done, ldRegx1, ldRegx2, ldRegT, ldRegW1, ldRegW2, ldRegB, ldRegFlag, counterReset, flagReset);
+module Controller(clk, rst, start, dataReady, endFlag, yEqualt, flagEOF, done, requestFlag,ldRegx1, ldRegx2, ldRegT, ldRegW1, ldRegW2, ldRegB, ldRegFlag, counterReset, flagReset);
 
-	parameter [2:0] start = 0, init = 1, getData = 2, calculate = 3, changeWeight = 4, checkEndFlag = 5, resetingCounter = 6;
+	input clk, rst, start, dataReady, endFlag, yEqualt, flagEOF;
+	output done, requestFlag,ldRegx1, ldRegx2, ldRegT, ldRegW1, ldRegW2, ldRegB, ldRegFlag, counterReset, flagReset;
+
+	parameter [2:0] start = 0, init = 1, requestData = 2, getData = 3, calculate = 4, changeWeight = 5, checkEndFlag = 6, resetingCounter = 7;
   reg [2:0] ns = 0, ps = 0;
 
   always @ ( posedge clk, posedge rst ) begin
@@ -24,11 +27,12 @@ module Controller(clk, rst, start, endFlag, yEqualt, flagEOF, done, ldRegx1, ldR
 		ldRegFlag = 1'b0;
 		counterReset = 1'b0;
 		flagReset = 1'b0;
-
+		requestFlag = 1'b0;
 
     case (ps)
       start: done = 1;
       init: reset = 1, counterReset = 1, flagReset = 1;
+			requestData: requestFlag = 1;
       getData: ldRegx1 = 1, ldRegx2 = 1, ldRegT = 1, counterEn = 1;
 			calculate: ldRegFlag = 1;
       changeWeight: ldRegW1 = 1, ldRegW2 = 1, ldRegB = 1;
@@ -42,12 +46,13 @@ module Controller(clk, rst, start, endFlag, yEqualt, flagEOF, done, ldRegx1, ldR
     ns = start;
     case (ps)
       start: ns = (start == 1'b1)? init : start;
-      init: ns = getData;
+      init: ns = requestData;
+			requestData: ns = (dataReady == 1'b1)? getData : requestData;
       getData: ns = calculate;
-			calculate: ns = (!flagEOF == 1'b1) ? ((!yEqualt == 1'b1) ? changeWeight : getData ) : checkEndFlag;
-      changeWeight: ns = getData;
+			calculate: ns = (!flagEOF == 1'b1) ? ((!yEqualt == 1'b1) ? changeWeight : requestData ) : checkEndFlag;
+      changeWeight: ns = requestData;
       checkEndFlag: ns = (endFlag == 1'b1)? resetingCounter : start;
-      resetingCounter: ns = getData;
+      resetingCounter: ns = requestData;
     endcase
   end
 
--- university/Computer Architecture/CA1/src/NeuronTB.v
+++ university/Computer Architecture/CA1/src/NeuronTB.v
@@ -1,1 +1,5 @@
+module Neuron_TB;
 
+reg clk, reset;
+Controller cu(clk, rst, start, endFlag, yEqualt, flagEOF, done, ldRegx1, ldRegx2, ldRegT, ldRegW1, ldRegW2, ldRegB, ldRegFlag, counterReset, flagReset);
+NeuronDP dp(clk, rst, reset, nInput, x1Input, x2Input, tInput, ldRegN, ldRegx1, ldRegx2, ldRegT, ldRegW1, ldRegW2, ldRegB, ldRegFlag, counterEn, flagReset, counterReset, endFlag, yEqualt, flagEOF);
