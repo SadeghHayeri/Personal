5fd874ecf9628ca38ba69998b63748d6f5d65e9b
SadeghHayeri <hayerisadegh@gmail.com>
2017-03-18 22:57:04 +0330

    denoised!

--- university/Engineering Mathematics/CA1/1.m
+++ university/Engineering Mathematics/CA1/1.m
@@ -8,12 +8,14 @@ plot(n, X)
 
 % b)
 fX = fftshift( fft(X) )
-plot(n, fX)
+% plot(n, fX)
 
 % d)
 fX(451) = 0;
 fX(551) = 0;
-plot(n, fX)
+fX(401) = 0;
+fX(601) = 0;
+% plot(n, abs(fX))
 
 % e)
 iX = ifft( fftshift(fX) );
