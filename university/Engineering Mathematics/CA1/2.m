5de4f579427b4c4150fba6b8d24bca1814f2f91b
SadeghHayeri <hayerisadegh@gmail.com>
2017-03-19 14:05:00 +0330

    noise completely removed!

--- university/Engineering Mathematics/CA1/2.m
+++ university/Engineering Mathematics/CA1/2.m
@@ -30,20 +30,22 @@ end
 % e)
 f = fftshift( fft(y) );
 absF = abs(f);
+% plot( n, absF );
 
-noisyLess = 0;
+noisyLess = f;
 for R = 1:l
     if absF(R) < 10000
-        noisyLess(end+1) = absF(R);
+        noisyLess(R) = f(R);
     else
-        noisyLess(end+1) = 0.000;
+        noisyLess(R) = 0.000;
     end
 end
-% plot( (0:length(noisyLess)-1), noisyLess);
 
-% noisyLess = fftshift(noisyLess);
-% noisyLess = ifft( noisyLess );
-% sound( noisyLess, Fs );
-plot( (0:length(noisyLess)-1), noisyLess);
+plot( (0:length(noisyLess)-1), abs(noisyLess));
+
+noisyLess = fftshift(noisyLess);
+noisyLess = ifft( noisyLess );
+sound( noisyLess, Fs );
+% plot( (0:length(noisyLess)-1), noisyLess);
 
 % http://stackoverflow.com/questions/24195089/remove-noise-from-wav-file-matlabs
