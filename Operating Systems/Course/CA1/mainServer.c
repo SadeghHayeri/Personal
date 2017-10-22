f3c8e8ca1fe2270057c1ba779f4acc7c65de732f
SadeghHayeri <hayerisadegh@gmail.com>
2017-10-22 13:51:17 +0330

    ready to upgrade!

--- Operating Systems/Course/CA1/mainServer.c
+++ Operating Systems/Course/CA1/mainServer.c
@@ -1,96 +1,143 @@
+/*
+** server.c -- a stream socket server demo
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
 
-#include "linkedlist.h"
-#include "structs.h"
+#define PORT "4000"  // the port users will be connecting to
 
-int _add_contributer(Node** contributers_head, int file_index, char ip[IP_LEN], int port) {
+#define BACKLOG 10     // how many pending connections queue will hold
 
-    int file_index_not_exist = (get_node(*contributers_head, file_index) == NULL);
-    if(file_index_not_exist) {
+void sigchld_handler(int s)
+{
+    // waitpid() might overwrite errno, so we save and restore it:
+    int saved_errno = errno;
 
-        add_node(contributers_head, file_index, ip, port);
+    while(waitpid(-1, NULL, WNOHANG) > 0);
+
+    errno = saved_errno;
+}
+
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2) {
+        fprintf(stderr,"usage: ./mainServer port\n");
+        exit(1);
+    }
+    char* hostname = argv[1];
+
+    int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
+    struct addrinfo hints, *servinfo, *p;
+    struct sockaddr_storage their_addr; // connector's address information
+    socklen_t sin_size;
+    struct sigaction sa;
+    int yes=1;
+    char s[INET6_ADDRSTRLEN];
+    int rv;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE; // use my IP
+
+    if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
         return 1;
-
-    } else {
-        return 0;
     }
 
-}
-
-// find file or create new and add a contributer to it
-// -1   ERR: file stack is full
-// 0    ERR: file_index already exist
-// 1    DONE
-int add_file_piece(File files[], char file_name[MAX_FILE_NAME], int file_index, char ip[IP_LEN], int port) {
-
-    for (size_t i = 0; i < MAX_FILE_HANDLER; i++) {
-
-        // if file exist -> add
-        int is_this_file = (strcmp(files[i].name, file_name) == 0);
-        if(is_this_file)
-            return _add_contributer(&files[i].contributers_head, file_index, ip, port);
-
-        // if not -> create & add
-        int is_empty_block = (strcmp(files[i].name, NOTSET) == 0);
-        if(is_empty_block) {
-            strcpy(files[i].name, file_name);
-            return _add_contributer(&files[i].contributers_head, file_index, ip, port);
+    // loop through all the results and bind to the first we can
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                p->ai_protocol)) == -1) {
+            perror("server: socket");
+            continue;
         }
 
-    }
-
-    return -1;
-}
-
-void print_files(File files[]) {
-    for (size_t i = 0; i < MAX_FILE_HANDLER; i++) {
-        int is_empty_block = (strcmp(files[i].name, NOTSET) == 0);
-        if(is_empty_block)
-            break;
-
-        printf("%lu %s\n", i+1, files[i].name);
-    }
-}
-
-void print_file_details(File files[], char file_name[MAX_FILE_NAME]) {
-    for (size_t i = 0; i < MAX_FILE_HANDLER; i++) {
-
-        // if file exist -> show
-        int is_this_file = (strcmp(files[i].name, file_name) == 0);
-        if(is_this_file) {
-            printf("%s:\n", file_name);
-
-            Node* curr_contributer = files[i].contributers_head;
-            while (curr_contributer != NULL) {
-                printf("(%d) %s:%d\n", curr_contributer->file_index, curr_contributer->ip, curr_contributer->port);
-                curr_contributer = curr_contributer->next;
-            }
-
-            return;
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+                sizeof(int)) == -1) {
+            perror("setsockopt");
+            exit(1);
         }
 
-        // if not -> error
-        int is_empty_block = (strcmp(files[i].name, NOTSET) == 0);
-        if(is_empty_block) {
-            printf("Not Found!");
-            return;
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("server: bind");
+            continue;
         }
 
+        break;
     }
-}
 
-int main() {
+    freeaddrinfo(servinfo); // all done with this structure
 
-    File files[MAX_FILE_HANDLER];
-    for (size_t i = 0; i < MAX_FILE_HANDLER; i++)
-        strcpy(files[i].name, NOTSET);
+    if (p == NULL)  {
+        fprintf(stderr, "server: failed to bind\n");
+        exit(1);
+    }
 
-    add_file_piece(files, "milad", 0, "172.0.0.1", 1000);
-    add_file_piece(files, "sadegh", 2, "172.0.0.1", 1000);
-    add_file_piece(files, "asghar", 1, "172.0.0.1", 1000);
-    add_file_piece(files, "milad", 3, "172.0.0.1", 1000);
+    if (listen(sockfd, BACKLOG) == -1) {
+        perror("listen");
+        exit(1);
+    }
 
-    // print_files(files);
-    print_file_details(files, "milad");
+    sa.sa_handler = sigchld_handler; // reap all dead processes
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+        perror("sigaction");
+        exit(1);
+    }
 
+    printf("server: waiting for connections...\n");
+
+    while(1) {  // main accept() loop
+        sin_size = sizeof their_addr;
+        new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+        if (new_fd == -1) {
+            perror("accept");
+            continue;
+        }
+
+        inet_ntop(their_addr.ss_family,
+            get_in_addr((struct sockaddr *)&their_addr),
+            s, sizeof s);
+        printf("server: got connection from %s\n", s);
+
+        if (!fork()) { // this is the child process
+            close(sockfd); // child doesn't need the listener
+
+            char buf[100];
+            recv(new_fd, buf, 100-1, 0);
+            write(0, buf, strlen(buf));
+            if (send(new_fd, buf, 100-1, 0) == -1)
+                perror("send");
+            close(new_fd);
+            exit(0);
+        }
+        close(new_fd);  // parent doesn't need this
+    }
+
+    return 0;
 }
--- Operating Systems/Course/CA1/server.c
+++ Operating Systems/Course/CA1/server.c
@@ -1,137 +1,55 @@
-/*
-** server.c -- a stream socket server demo
-*/
+#include "structs.h"
+#include "network.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <arpa/inet.h>
-#include <sys/wait.h>
-#include <signal.h>
+// ADD_FILE|<name>|<part_num>
+char* get_initial_command(char* name, char* part) {
+    char* header = (char*)malloc(MAX_DATA_SIZE * sizeof(char));
+    char* end_char = header;
 
-#define PORT "4000"  // the port users will be connecting to
+    strcpy(end_char, HEADER_ADD_FILE);
+    end_char += strlen(HEADER_ADD_FILE);
 
-#define BACKLOG 10     // how many pending connections queue will hold
+    strcpy(end_char, HEADER_SEPERATOR);
+    end_char += strlen(HEADER_SEPERATOR);
 
-void sigchld_handler(int s)
-{
-    // waitpid() might overwrite errno, so we save and restore it:
-    int saved_errno = errno;
+    strcpy(end_char, name);
+    end_char += strlen(name);
 
-    while(waitpid(-1, NULL, WNOHANG) > 0);
+    strcpy(end_char, HEADER_SEPERATOR);
+    end_char += strlen(HEADER_SEPERATOR);
 
-    errno = saved_errno;
-}
+    strcpy(end_char, part);
+    end_char += strlen(part);
 
-
-// get sockaddr, IPv4 or IPv6:
-void *get_in_addr(struct sockaddr *sa)
-{
-    if (sa->sa_family == AF_INET) {
-        return &(((struct sockaddr_in*)sa)->sin_addr);
-    }
-
-    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+    return header;
 }
 
 int main(int argc, char *argv[])
 {
-    int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
-    struct addrinfo hints, *servinfo, *p;
-    struct sockaddr_storage their_addr; // connector's address information
-    socklen_t sin_size;
-    struct sigaction sa;
-    int yes=1;
-    char s[INET6_ADDRSTRLEN];
-    int rv;
 
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE; // use my IP
-
-    if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-        return 1;
+    if (argc != 6) {
+        fprintf(stderr,"usage: ./server hostname port file_path name part_num\n");
+        exit(1);
     }
+    char* hostname = argv[1];
+    char* port = argv[2];
+    char* file_path = argv[3];
+    char* name = argv[4];
+    char* part_num = argv[5];
 
-    // loop through all the results and bind to the first we can
-    for(p = servinfo; p != NULL; p = p->ai_next) {
-        if ((sockfd = socket(p->ai_family, p->ai_socktype,
-                p->ai_protocol)) == -1) {
-            perror("server: socket");
-            continue;
-        }
+    int file_fd = open(file_path, O_RDONLY);
+    if (file_fd == -1) {
 
-        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
-                sizeof(int)) == -1) {
-            perror("setsockopt");
-            exit(1);
-        }
-
-        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-            close(sockfd);
-            perror("server: bind");
-            continue;
-        }
-
-        break;
-    }
-
-    freeaddrinfo(servinfo); // all done with this structure
-
-    if (p == NULL)  {
-        fprintf(stderr, "server: failed to bind\n");
+        perror("File Not Found!");
         exit(1);
     }
 
-    if (listen(sockfd, BACKLOG) == -1) {
-        perror("listen");
-        exit(1);
-    }
+    int sock_fd = create_socket_fd(hostname, port);
 
-    sa.sa_handler = sigchld_handler; // reap all dead processes
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = SA_RESTART;
-    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
-        perror("sigaction");
-        exit(1);
-    }
+    char* header = get_initial_command(name, part_num);
+    char* response = request(sock_fd, header);
+    write(0, response, strlen(response));
 
-    printf("server: waiting for connections...\n");
-
-    while(1) {  // main accept() loop
-        sin_size = sizeof their_addr;
-        new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
-        if (new_fd == -1) {
-            perror("accept");
-            continue;
-        }
-
-        inet_ntop(their_addr.ss_family,
-            get_in_addr((struct sockaddr *)&their_addr),
-            s, sizeof s);
-        printf("server: got connection from %s\n", s);
-
-        if (!fork()) { // this is the child process
-            close(sockfd); // child doesn't need the listener
-
-            char buf[100];
-            recv(new_fd, buf, 100-1, 0);
-            write(0, buf, strlen(buf));
-            if (send(new_fd, buf, 100-1, 0) == -1)
-                perror("send");
-            close(new_fd);
-            exit(0);
-        }
-        close(new_fd);  // parent doesn't need this
-    }
-
+    close(sock_fd);
     return 0;
 }
