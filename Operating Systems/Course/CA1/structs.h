195d1a8a68394f5e791b3b88c191ef8869980c17
SadeghHayeri <hayerisadegh@gmail.com>
2017-10-24 00:37:49 +0330

    bugs fixed

--- Operating Systems/Course/CA1/structs.h
+++ Operating Systems/Course/CA1/structs.h
@@ -4,20 +4,23 @@
 #define MAX_FILE_NAME 50
 #define MAX_FILE_HANDLER 10
 #define NOTSET "NOTSET"
-#define IP_LEN 15   // worst case: 255.255.255.255
-
-
+#define IP_LEN 22   // worst case: ::ffff:255.255.255.255
+#define PORT_LEN 15   // worst case: 60000
+#define MAX_RANDOM_PORT 60000
+#define RANDOM_PORT_OFFSET 1000
 
 #define MAX_DATA_SIZE 100
+#define OK_MESSAGE "OK"
 #define HEADER_ADD_FILE "ADD_FILE"
 #define HEADER_SEPERATOR "|"
 
 
+#define BACKLOG 10     // how many pending connections queue will hold
 
 typedef struct node {
     int file_index;
     char ip[IP_LEN];
-    int port;
+    char port[PORT_LEN];
 
     struct node *next;
 } Node;
