83c226ceb9604211ada4f67741cb5297d219b4a7
SadeghHayeri <hayerisadegh@gmail.com>
2017-10-28 09:39:08 +0330

    make clean!

--- Operating Systems/Course/CA1/structs.h
+++ Operating Systems/Course/CA1/structs.h
@@ -1,35 +1,50 @@
 #ifndef STRUCTS_H
 #define STRUCTS_H
 
+// I/O file descriptors
 #define STDIN 0
 #define STDOUT 1
 #define STDERR 2
 
+// MainServer file handler
 #define MAX_FILE_NAME 50
 #define MAX_FILE_HANDLER 10
-#define NOTSET "NOT_SET"
-#define NOT_FOUND "NOT_FOUND"
-#define OK_MESSAGE "OK"
-#define ERROR_MESSAGE "ERR"
-#define BAD_COMMAND "BAD_COMMAND"
+
+// Address info
 #define IP_LEN 22   // worst case: ::ffff:255.255.255.255
 #define PORT_LEN 15   // worst case: 60000
 #define MAX_RANDOM_PORT 60000
 #define RANDOM_PORT_OFFSET 1000
 
+// Configs
 #define MAX_DATA_SIZE 700
 #define CHUNK_SIZE 500
+#define BACKLOG 10     // how many pending connections queue will hold
 
-#define HEADER_ADD_FILE "ADD_FILE"
-#define HEADER_GET_CHUNK_COUNT "GET_CHUNK_COUNT"
-#define HEADER_GET_CHUNK "GET_CHUNK"
-#define HEADER_GET_COUNTRIBUTERS "GET_COUNTRIBUTERS"
-#define DATA_MARKER "DATA"
+
+/////////////////////// API ////////////////////////
+// Separators
 #define HEADER_SEPERATOR "<*>"
 #define HEADER_SUB_SEPERATOR "<|>"
 
+// Client Requests
+#define HEADER_GET_CHUNK_COUNT "GET_CHUNK_COUNT"
+#define HEADER_GET_CHUNK "GET_CHUNK"
+#define HEADER_GET_COUNTRIBUTERS "GET_COUNTRIBUTERS"
 
-#define BACKLOG 10     // how many pending connections queue will hold
+// Server Requests
+#define HEADER_ADD_FILE "ADD_FILE"
+
+// Server Responses
+#define DATA_MARKER "DATA"
+
+// MainServer Responses
+#define OK_MESSAGE "OK"
+#define ERROR_MESSAGE "ERR"
+#define BAD_COMMAND "BAD_COMMAND"
+#define NOTSET "NOT_SET"
+#define NOT_FOUND "NOT_FOUND"
+////////////////////////////////////////////////////
 
 typedef struct Node {
 	int contributer_id;
@@ -40,7 +55,7 @@ typedef struct Node {
     struct Node *next;
 } Node;
 
-typedef struct file {
+typedef struct File {
     char name[MAX_FILE_NAME];
     Node* contributers_head;
 } File;
