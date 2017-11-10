00dd3a8e95ad84be552e2f51ffe718553ba0902d
SadeghHayeri <hayerisadegh@gmail.com>
2017-11-10 11:17:43 +0330

    server complete!

---
+++ Operating Systems/Course/CA2/server.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include <fstream>
+#include <unistd.h>
+
+#include <time.h>
+
+#include "utility.h"
+#include "pipe.h"
+
+using namespace std;
+
+void search_on_file(string path, int id, Unamed_pipe& pipe) {
+
+	fstream file;
+	file.open(path);
+
+	long long all_amount = 0;
+	while(!file.eof()) {
+		int tmp_id;
+		string tmp_date;
+		long long tmp_amount;
+
+		file >> tmp_id >> tmp_date >> tmp_amount;
+
+		if(tmp_id == id)
+			all_amount += tmp_amount;
+	}
+
+	cout << ">>" << path << " - " << all_amount << "<<" << endl;
+
+	pipe << to_string(all_amount);
+	file.close();
+}
+
+void find_all_in_directory(string path, int id, Unamed_pipe& pipe) {
+	vector<string> files = Utility::get_files(path);
+	vector<string> directories = Utility::get_directories(path);
+
+	int num_of_pipe = files.size() + directories.size();
+	int pipe_index = 0;
+	vector<Unamed_pipe> pipes;
+	for(int i = 0; i < num_of_pipe; i++)
+		pipes.push_back(Unamed_pipe());
+
+	for(string directory : directories)
+		if(!fork()) {
+			find_all_in_directory(path + '/' + directory, id, pipes[pipe_index]);
+			exit(0);
+		} else 
+			pipe_index++;
+
+	for(string file : files)
+		if(!fork()) {
+			search_on_file(path + "/" + file, id, pipes[pipe_index]);
+			exit(0);
+		} else 
+			pipe_index++;
+
+	for(int i = 0; i < num_of_pipe; i++)
+		wait(NULL);
+
+	long long all_amount = 0;
+	for(Unamed_pipe& child_pipe : pipes) {
+		string amount_string;
+		child_pipe >> amount_string;
+
+		cout << "::" << path << " - " << amount_string << "::" << endl;
+		all_amount += stoll(amount_string);
+	}
+
+	pipe << to_string(all_amount);
+}
+
+
+
+int main() {
+
+	Unamed_pipe p = Unamed_pipe();
+	find_all_in_directory("Data 2", 76767, p);
+
+	string res;
+	p >> res;
+	cout << "result: " << res << endl;
+	
+}
