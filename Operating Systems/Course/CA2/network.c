611d4ced9a4e0f65762cc3a86ab5c53e1f64d446
SadeghHayeri <hayerisadegh@gmail.com>
2017-11-12 12:08:52 +0330

    network added!

---
+++ Operating Systems/Course/CA2/network.c
@@ -0,0 +1,226 @@
+#include "network.h"
+#include "utility.h"
+#include "structs.h"
+#include "logger.h"
+
+void request(Max_size_data response, int sock_fd, char* msg) {
+    memset(response, '\0', MAX_DATA_SIZE);
+    send(sock_fd, msg, strlen(msg), 0);
+    recv(sock_fd, response, MAX_DATA_SIZE, 0);
+}
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+void *get_in_port(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_port);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_port);
+}
+
+int create_socket_fd(char* ip, char* port) {
+    int sock_fd;
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
+    char s[INET6_ADDRSTRLEN];
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+    if ((rv = getaddrinfo(ip, port, &hints, &servinfo)) != 0) {
+        error("getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+
+    // loop through all the results and connect to the first we can
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sock_fd = socket(p->ai_family, p->ai_socktype,
+                p->ai_protocol)) == -1) {
+            error("client: socket");
+            continue;
+        }
+
+        if (connect(sock_fd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sock_fd);
+            error("client: connect");
+            continue;
+        }
+        break;
+    }
+
+    if (p == NULL) {
+        error("client: failed to connect");
+        return 2;
+    }
+
+    inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
+            s, sizeof s);
+
+    freeaddrinfo(servinfo); // all done with this structure
+
+    return sock_fd;
+}
+
+int create_listener_fd(char* port) {
+    int listener;     // listening socket descriptor
+    socklen_t addrlen;
+
+    int yes=1;        // for setsockopt() SO_REUSEADDR, below
+    int rv;
+
+    struct addrinfo hints, *ai, *p;
+
+    // get us a socket and bind it
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+    if ((rv = getaddrinfo(NULL, port, &hints, &ai)) != 0) {
+        error("selectserver: %s\n", gai_strerror(rv));
+        exit(1);
+    }
+
+    for(p = ai; p != NULL; p = p->ai_next) {
+        listener = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+        if (listener < 0) {
+            continue;
+        }
+
+        // lose the pesky "address already in use" error message
+        setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
+
+        if (bind(listener, p->ai_addr, p->ai_addrlen) < 0) {
+            close(listener);
+            continue;
+        }
+
+        break;
+    }
+
+    // if we got here, it means we didn't get bound
+    if (p == NULL) {
+        error("selectserver: failed to bind");
+        exit(2);
+    }
+
+    freeaddrinfo(ai); // all done with this
+
+    // listen
+    if (listen(listener, 10) == -1) {
+        error("listen");
+        exit(3);
+    }
+
+    return listener;
+}
+
+void listen_to_clients(
+    int listener,
+    char* listener_port,
+    void (*request_handler)(Max_size_data result, int id, char* ip, char* req),
+    void (*disconnect_handler)(int id)
+) {
+
+    fd_set master;    // master file descriptor list
+    fd_set read_fds;  // temp file descriptor list for select()
+    int fdmax;        // maximum file descriptor number
+
+    socklen_t addrlen;
+    struct sockaddr_storage remoteaddr; // client address
+    int newfd;        // newly accept()ed socket descriptor
+    char remoteIP[INET6_ADDRSTRLEN];
+    char buf[MAX_DATA_SIZE];    // buffer for client data
+    memset(buf, '\0', MAX_DATA_SIZE);
+    int nbytes;
+
+    // clear the master and temp sets
+    FD_ZERO(&master);
+    FD_ZERO(&read_fds);
+
+    // add the listener to the master set
+    FD_SET(listener, &master);
+
+    // keep track of the biggest file descriptor
+    fdmax = listener; // so far, it's this one
+
+    // TODO: pak kon ya ye jaye dg
+    // Listening to clients
+    logger("Listening on port: %s", listener_port);
+
+    // main loop
+    for(;;) {
+        read_fds = master; // copy it
+        if (select(fdmax+1, &read_fds, NULL, NULL, NULL) == -1) {
+            error("select");
+            exit(4);
+        }
+
+        // run through the existing connections looking for data to read
+        for(int i = 0; i <= fdmax; i++) {
+            if (FD_ISSET(i, &read_fds)) { // we got one!!
+                if (i == listener) {
+                    // handle new connections
+                    addrlen = sizeof remoteaddr;
+                    newfd = accept(listener,
+                        (struct sockaddr *)&remoteaddr,
+                        &addrlen);
+
+                    if (newfd == -1) {
+                        error("accept");
+                    } else {
+                        FD_SET(newfd, &master); // add to master set
+                        if (newfd > fdmax) {    // keep track of the max
+                            fdmax = newfd;
+                        }
+                        logger("selectserver: new connection from %s on "
+                            "socket %d\n",
+                            inet_ntop(remoteaddr.ss_family,
+                                get_in_addr((struct sockaddr*)&remoteaddr),
+                                remoteIP, INET6_ADDRSTRLEN),
+                            newfd);
+
+                    }
+                } else {
+                    // handle data from a client
+                    if ((nbytes = recv(i, buf, sizeof buf, 0)) <= 0) {
+                        // got error or connection closed by client
+                        if (nbytes == 0) {
+                            // connection closed
+                            printf("selectserver: socket %d hung up\n", i);
+                        } else {
+                            error("recv");
+                        }
+                        disconnect_handler(i);
+                        close(i); // bye!
+                        FD_CLR(i, &master); // remove from master set
+                    } else {
+
+                        char* ip = (char*)inet_ntop(remoteaddr.ss_family,
+                            get_in_addr((struct sockaddr*)&remoteaddr),
+                            remoteIP, INET6_ADDRSTRLEN);
+
+                        // char* response = request_handler(i, ip, buf);
+                        char response[MAX_DATA_SIZE];
+                        request_handler(response, i, ip, buf);
+                        logger("REQ: %s", buf);
+                        logger("RES: %s", response);
+                        send(i, response, strlen(response), 0);
+                        memset(buf, '\0', MAX_DATA_SIZE);
+
+                    }
+                } // END handle data from client
+            } // END got new incoming connection
+        } // END looping through file descriptors
+    } // END for(;;)--and you thought it would never end!
+}
---
+++ Operating Systems/Course/CA2/network.h
@@ -0,0 +1,52 @@
+#ifndef NETWORK_H
+#define NETWORK_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+#include <fcntl.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include <arpa/inet.h>
+
+#include "structs.h"
+
+void *get_in_addr(struct sockaddr *sa);
+int create_socket_fd(char* ip, char* port);
+int create_listener_fd(char* port);
+void request(Max_size_data response, int sock_fd, char* msg);
+
+void sigchld_handler(int s);
+void listen_to_clients(
+    int listener,
+    char* listener_port,
+    void (*request_handler)(Max_size_data result, int id, char* ip, char* req),
+    void (*disconnect_handler)(int id)
+);
+
+
+
+#endif
